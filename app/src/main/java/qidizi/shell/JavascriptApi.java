89cdada66de50fbd9b776612d3b4a371d5787ef6
qidizi <qidizi@qq.com>
2018-08-21 09:27:37 +0800

    test

---
+++ app/src/main/java/qidizi/shell/JavascriptApi.java
@@ -0,0 +1,148 @@
+package qidizi.shell;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.webkit.JavascriptInterface;
+import android.widget.Toast;
+import java.io.InputStream;
+import java.lang.ref.WeakReference;
+import java.net.URL;
+import java.io.*;
+import android.webkit.*;
+import org.json.*;
+
+/**
+ * 实现js api给web调用
+ */
+class JavascriptApi
+{
+    private browser myBrowser;
+
+    /**
+     * Instantiate the interface and set the context
+     */
+    JavascriptApi(browser c)
+    {
+        myBrowser = c;
+    }
+
+    @JavascriptInterface
+    public void bash(
+        String... args
+    )
+    {
+        WechatShareTask task = new WechatShareTask(myBrowser);
+        // 如果 thumb 为空字符串，doInBackground就不会执行
+        task.execute(args);
+    }
+
+
+
+
+    private void showToast(String text)
+    {
+        Toast.makeText(myBrowser, text, Toast.LENGTH_LONG).show();
+    }
+    
+    /**
+     * <参数类型,进度值,任务最终值>
+     */
+    static private class WechatShareTask extends AsyncTask<String[], Void, String[]>
+    {
+        // 使用弱引用方式，防止内存漏泄
+        final private WeakReference<browser> myBrowser;
+
+        WechatShareTask(browser br)
+        {
+            this.myBrowser = new WeakReference<browser>(br);
+        }
+        
+        /**
+         * 它的参数是通过new Task().execute(参数)传递的
+         */
+        protected String[] doInBackground(String[]... params)
+        {
+            String[] result = new String[]{"成功","success"};
+
+            try
+            {
+                String[] env=new String[]{""};
+                File dir = new File("/data/data/qidizi.shell");
+                Process ps =Runtime.getRuntime().exec(params[0], env, dir);
+
+                //这句话就是shell与高级语言间的调用
+                //如果有参数的话可以用另外一个被重载的exec方法
+                //实际上这样执行时启动了一个子进程,它没有父进程的控制台
+                //也就看不到输出,所以我们需要用输出流来得到shell执行后的输出
+                InputStream inputstream = ps.getInputStream();
+                InputStreamReader inputstreamreader = new InputStreamReader(inputstream);
+                BufferedReader bufferedreader = new BufferedReader(inputstreamreader);
+                // read the ls output
+                String line = "";
+                StringBuilder sb = new StringBuilder(line);
+                while ((line = bufferedreader.readLine()) != null)
+                {
+                    //System.out.println(line);
+                    sb.append(line);
+                    sb.append('\n');
+                }
+                //tv.setText(sb.toString());
+                //使用exec执行不会等执行成功以后才返回,它会立即返回
+                //所以在某些情况下是很要命的(比如复制文件的时候)
+                //使用wairFor()可以等待命令执行完成以后才返回
+                if (ps.waitFor() != 0)
+                {
+                    System.err.println("exit value = " + ps.exitValue());
+                } 
+                result[0] = sb.toString();
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+                result[1] = "bash 运行失败：" + e.getMessage();
+                result[0] = "fail";
+            }
+
+            return result;
+        }
+
+        /**
+         * resut 来自 doInBackground return
+         * ui 更新只能在这里处理
+         * 注意，只有签名与状态为发布的才行，否则就会闪下
+         */
+        protected void onPostExecute(String[] result)
+        {
+            WebView web = myBrowser.get().getWebView();
+
+            if (null == web)
+            {
+                showToast("无法获取webview");
+                return;
+            }
+
+           JSONObject json = new JSONObject();
+           String arg = "";
+           
+           try{
+           for (int i = 0;i < result.length;i++){
+               json.put(i+"",result[i]);
+           }
+           
+           arg = json.toString();
+           }catch(Exception e){
+               arg = "获取结果异常";
+           }
+            web.loadUrl("javascript:try{" +
+                "myAlert(" + arg + ");" +
+                "} catch(e) { alert('apk 执行js异常:\\n' + e); }");
+        }
+
+        private void showToast(String text)
+        {
+            Toast.makeText(myBrowser.get(), text, Toast.LENGTH_LONG).show();
+        }
+    }
+}
---
+++ app/src/main/java/qidizi/shell/browser.java
@@ -0,0 +1,180 @@
+package qidizi.shell;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.*;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+import android.*;
+import android.content.pm.*;
+import android.os.*;
+import java.io.*;
+
+public class browser extends Activity
+{
+    private WebView webView;
+    final static private int  MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        // 修改标题栏左边logo与app不同
+        //requestWindowFeature(Window.FEATURE_LEFT_ICON);
+        //getWindow().setFeatureDrawableResource(Window.FEATURE_LEFT_ICON, R.drawable.status_bar_logo);
+        webView = new WebView(this);
+        // 加载进度显示在标题栏上
+        getWindow().requestFeature(Window.FEATURE_PROGRESS);
+        webView.setWebContentsDebuggingEnabled(true);
+        //设置WebView属性，能够执行Javascript脚本
+        webView.getSettings().setJavaScriptEnabled(true);
+        // 启用 window.localStorage
+        webView.getSettings().setDomStorageEnabled(true);
+        //webView.getSettings().setDefaultTextEncodingName("UTF-8");
+        //webView.getSettings().setSaveFormData(true);
+        webView.setWebChromeClient(new WebChromeClient() {
+                @Override
+                public void onProgressChanged(WebView view, int newProgress)
+                {
+                    // 显示实时进度
+                    setProgress(newProgress * 100);
+                    super.onProgressChanged(view, newProgress);
+                }
+            });
+        webView.getSettings().setUserAgentString(webView.getSettings().getUserAgentString() + " iGuoYiSiteAppApk/20180808");
+        // WebChromeClient 这个是用来handle js的调用，类似于js的debugger，比如获得所有js的调用结果
+        // 解决这个问题： Denied starting an intent without a user gesture, URI http://www.iguoyi.cn/；这个是在页面html render完成时回调
+        webView.setWebViewClient(new WebViewClient() {
+                @Override
+                public void onPageFinished(WebView view, String url)
+                {
+                    // 页面加载完成，显示当前页标题
+                    setTitle(webView.getTitle());
+                    super.onPageFinished(view, url);
+                }
+
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, String url)
+                {
+                    //false 表示点击的url交webview来处理，而不是外面浏览器，但是不会拦post请求
+                    return false;
+                }
+            });
+        webView.addJavascriptInterface(new JavascriptApi(this), "apk");
+        setContentView(webView);
+        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED)
+        {
+
+            // Should we show an explanation?
+            if (shouldShowRequestPermissionRationale(
+                    Manifest.permission.READ_EXTERNAL_STORAGE))
+            {
+                // Explain to the user why we need to read the contacts
+            }
+
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                               MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+
+            // MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE is an
+            // app-defined int constant that should be quite unique
+
+            return;
+        }
+        String path = "file://"+Environment.getExternalStorageDirectory().toString() +File.separator+ getString(R.string.htmlPath);
+         webView.loadUrl(path);
+
+    }
+
+    /**
+     * 获取webView
+     *
+     * @return WebView
+     */
+    public WebView getWebView()
+    {
+        return webView;
+    }
+
+    /**
+     * 绑定标题栏上右上角菜单显示
+     *
+     * @param menu 菜单
+     * @return boolean
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /**
+     * 标题栏上右上角菜单点击处理
+     *
+     * @param item
+     * @return
+     */
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.help:
+                // 打开国艺首页
+                Toast.makeText(this, R.string.help, Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.refresh:
+                webView.reload();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /**
+     * 点击硬件返回键时，如果还能继续后退就执行后退动作，否则弹出结束对话框
+     *
+     * @param keyCode 按键
+     * @param event   事件
+     * @return bool
+     */
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack())
+        {
+            webView.goBack(); //goBack()表示返回WebView的上一页面
+            return true;
+        }
+        // 不允许直接退出程序
+
+        new AlertDialog.Builder(this).setTitle("确定退出吗？")
+            .setIcon(android.R.drawable.ic_dialog_info)
+            .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which)
+                {
+                    // 点击“确认”后的操作
+                    browser.this.finish();
+                }
+            })
+            .setNegativeButton("点错了", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which)
+                {
+                    // 点击“返回”后的操作,这里不设置没有任何操作
+                }
+            }).show();
+        return false;
+    }
+}
